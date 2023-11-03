0f779200990358db12ab0d04936996099da44cbd
ilya.maksimov <ilya.maksimov>
2023-11-04 00:26:43 +0100

    steamcmd installation

--- ansible/examples.sh
+++ ansible/examples.sh
@@ -1,4 +1,6 @@
 ansible-playbook -i inventory.aws_ec2.yml cs2server.yml \
     -e "region=eu-central-1" \
     -e "reqOwner=project1" \
-    -e "reqName=mycs2server"
+    -e "reqName=mycs2server" \
+    -e "steamUser=" \
+    -e "steamPass=" \
